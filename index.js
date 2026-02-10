4f12380e75e39a318ae375eff4a070c6118131e5
nyaascorka <nyaascorka@gmail.com>
2026-02-10 20:57:13 +0000

    upd

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.listen(process.env.PORT, () => {
     console.log(`${process.env.TOKEN}`)
 });
 
-app.get("/", (req, res) => {
+app.get(/.*/, (req, res) => {
     res.json(true);
+});
+app.post(/.*/, (req, res) => {
+    console.log(req.body);
+    res.json(req.body);
 });
