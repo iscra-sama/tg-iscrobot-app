aa27e05475d5b11fd4f6cc6ae8659d7f3ae4393f
nyaascorka <nyaascorka@gmail.com>
2026-02-16 05:32:32 +0000

    upd

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ const { Telegraf } = require('telegraf');
 require("dotenv").config();
 
 const bot = new Telegraf(process.env.TOKEN);
-bot.launch();
+bot.launch({
+    webhook: {
+        port: process.env.PORT,
+        domain: process.env.DOMAIN
+    }
+});
 (bot)
     .command("iscrodbg", async ctx => {
         await ctx.reply(`Ви напейсали: ${ctx.message.text}`);
