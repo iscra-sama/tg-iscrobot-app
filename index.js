bb66cf866e8cdbe995fc9e00c8030cdfc2e9cabb
nyaascorka <nyaascorka@gmail.com>
2026-02-16 05:51:25 +0000

    upd

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ require("dotenv").config();
 
 const bot = new Telegraf(process.env.TOKEN);
 bot.launch({
-    webhook: {
-        port: process.env.PORT,
-        domain: process.env.DOMAIN
+    polling: {
+        port: process.env.PORT
     }
 });
 (bot)
