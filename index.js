16f9db4699c5961afbdb76e0153c7797448e5a3b
iscra-sama <nyaascorka@gmail.com>
2026-02-19 16:22:10 +0000

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ let isHuurmoonecMuted = false;
         let [_, flag, ...__] = (ctx.message.text).trim().split(" ");
 
         if (flag === undefined) {
-            return await ctx.reply(`Заблокировать сообщения Huurmōnec. На данный момент ${isHuurmoonecMuted ? "" : "не"} забанен. Команда доступна админам. Работает по принципу удаления каждого поступающего от него сообщения. Доки: \n 1) /iscromute -t (мут); \n 2) /iscromute -f (размут); \n 3) /iscromute (-n/-i/-c) (смена состояния на обратное). \n Синонимы аргументов команды: (-t/--true), (-f/--false), (-n/-i/-c/--neg/--not/--inv/--compl)`)
+            return await ctx.reply(`Заблокировать сообщения Huurmōnec. На данный момент ${isHuurmoonecMuted ? "" : "не"} забанен. Команда доступна админам. Работает по принципу автоудаления каждого поступающего от него сообщения. Доки: \n 1) «/iscromute -t» (мут); \n 2) «/iscromute -f» (размут); \n 3) «/iscromute -n» (смена состояния на обратное). \n У аргументов команды есть синонимы: \n 1) -t/--true; \n 2) -f/--false; 3) -n/-i/-c/--neg/--not/--inv/--compl.`)
         }
         switch (true) {
             case ctx.message.from.id === process.env.HUURMOONEC_ID:
